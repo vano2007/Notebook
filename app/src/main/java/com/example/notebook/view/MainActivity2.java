d5230fd4923376ef1ded1d524253c8bdcf07e6b7
Eduard Angelskiy <ed_angel74@mail.ru>
2023-04-26 12:26:01 +0700

    Commit_260423

--- app/src/main/java/com/example/notebook/view/MainActivity2.java
+++ app/src/main/java/com/example/notebook/view/MainActivity2.java
@@ -3,7 +3,9 @@ package com.example.notebook.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.notebook.R;
 import com.example.notebook.model.Notebook;
@@ -22,7 +24,7 @@ public class MainActivity2 extends AppCompatActivity {
     private List<Notebook> notesList; // поле для контейнера списка заметок
 
     private DatabaseHelper database; // поле работы с БД
-    private Adapter adapter; // поле для адаптера
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +37,35 @@ public class MainActivity2 extends AppCompatActivity {
 
         notesList = new ArrayList<>(); // выделение памяти и задание типа контейнера для списка заметок
         database = new DatabaseHelper(this); // выделение памяти и задание текущего контекста работы с БД
+
+        // считывание данных из БД и запись их в коллекцию notesList
+        fetchAllNotes();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); // задание структуры вывода данных в recyclerView
+        adapter = new Adapter(this, SecondActivity.this, notesList); // инициализация адаптера и передача в рего данных из БД
+        recyclerView.setAdapter(adapter); // передача в recyclerView адаптер
+
+        // обработка нажатия кнопки создания новой заметки
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // переключение на новую активность
+                startActivity(new Intent(SecondActivity.this, AddNotesActivity.class));
+            }
+        });
+    }
+    // метод считывания из БД всех записей
+    public void fetchAllNotes(){
+        // чтение БД и запись данных в курсор
+        Cursor cursor = database.readNotes();
+
+        if (cursor.getCount() == 0) { // если данные отсутствую, то вывод на экран об этом тоста
+            Toast.makeText(this, "Заметок нет", Toast.LENGTH_SHORT).show();
+        } else { // иначе помещение их в контейнер данных notesList
+            while (cursor.moveToNext()){
+                // помещение в контейнер notesList из курсора данных
+                notesList.add(new Notebook(cursor.getString(0), cursor.getString(1), cursor.getString(2)));
+            }
+        }
     }
 }
